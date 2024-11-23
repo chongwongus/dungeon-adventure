40ee0b57dbe3c80460e6d68570b31dbc8247bbd1
chongwongus <swagthunder69@gmail.com>
2024-11-23 04:51:29 -0800

    initial files created

---
+++ src/Adventurer.py
@@ -0,0 +1,5 @@
+class Adventurer:
+    def __init__(self, name, hp=100):
+        self.name = name
+        self.hp = hp
+
---
+++ src/Room.py
@@ -0,0 +1,8 @@
+class Room:
+    def __init__(self):
+        self.hasPit = False
+        self.hasHealingPotion = False
+        self.hasPillar = False
+        self.doors = {}
+
+
