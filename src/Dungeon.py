e85b9deb24ecb7170377261e9c32e265c7c377a1
chongwongus <swagthunder69@gmail.com>
2024-11-25 02:20:21 -0800

    UML diagram added as project-structure.mermaid

--- src/Dungeon.py
+++ src/Dungeon.py
@@ -1,4 +1,7 @@
-class Dungeon:
-    def __init__(self, size):
-        self.size = size
+import Room as Room
 
+class Dungeon:
+    def __init__(self, size=(8, 8)):
+        self.size = size
+        self.entrance = None
+        self.exit = None
